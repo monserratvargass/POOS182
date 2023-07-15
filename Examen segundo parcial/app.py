0fbe925a1add971b1c35b4914d11af472fb40bf0
monserratvargass <guvamonse@gmail.com>
2023-07-14 18:41:59 -0600

    Segundo commit fin del examen

--- Examen segundo parcial/app.py
+++ Examen segundo parcial/app.py
@@ -1,21 +1,23 @@
-from flask import Flask, render_template, request, redirect,url_for, flash
+#nombre.server.py o app.py
+from flask import Flask, render_template, request, redirect,url_for,flash #Importar libreria
 from flask_mysqldb import MySQL
 
-app=Flask(__name__)
+app=Flask(__name__) #Inicializacion del servidor Flask
 
-app.config['MySQL_HOST']="localhost"
-app.config['MySQL_USER']="root"
-app.config['MySQL_PASSWORD']=""
-app.config['MySQL_DB']="dbfloreria"
+#Configuraciones para la conexion de   
+app.config['MYSQL_HOST']="localhost" #Especificar en que servidor trabajamos
+app.config['MYSQL_USER']="root" #Especificar usuario
+app.config['MYSQL_PASSWORD']="" #Especificar contraseña
+app.config['MYSQL_DB']="dbfloreria" #Especificar a que base de datos
 
-app.secret_key='mysecretkey'
+app.secret_key='mysecretkey' #Permite hacer envios a traves de post
 
 mysql=MySQL(app)
 
 @app.route('/')
 def index():
     curSelect=mysql.connection.cursor()
-    curSelect.execute('select * from Floreria')
+    curSelect.execute('select * from tbflores')
     consulta=curSelect.fetchall()
     
     return render_template('index.html', Cfloreria=consulta)
@@ -28,12 +30,12 @@ def guardar():
         Nprecio=request.form['precio']
 
         CS=mysql.connection.cursor()
-        CS.execute('insert into floreria(nombre,cantidad,precio) values(%s,%s,%s)',(Nnombre,Ncantidad,Nprecio))
+        CS.execute('insert into tbflores(nombre,cantidad,precio) values(%s,%s,%s)',(Nnombre,Ncantidad,Nprecio))
         mysql.connection.commit()
     flash('Registro agregado')
     return redirect(url_for('index'))
 
-@app.route('/eliminar/<id>')
+@app.route('/eliminar/<id>',methods=['POST'])
 def eliminar(id):
     if request.method=='POST':
         Nnombre=request.form['nombre']
@@ -41,7 +43,7 @@ def eliminar(id):
         Nprecio=request.form['precio']
 
         CS=mysql.connection.cursor()
-        CS.execute('delete from floreria where id=%s',(id))
+        CS.execute('delete from tbflores where id=%s',(id))
         mysql.connection.commit()
     flash('Registro eliminado')
     return redirect(url_for('index'))
@@ -49,7 +51,7 @@ def eliminar(id):
 @app.route('/borrar/<id>')
 def borrar(id):
     curBorrar=mysql.connection.cursor()
-    curBorrar.execute('select * from floreria where id= %s', (id,))#Coma importante por que lo confunde con una tupla
+    curBorrar.execute('select * from tbflores where id= %s', (id,))#Coma importante por que lo confunde con una tupla
     consultaID=curBorrar.fetchone() #Para traer unicamente un registro
 
     return render_template('eliminarFloreria.html', floreria=consultaID)
